7ac1a98da14edeb510087b4266647dd94125499a
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-15 11:33:43 +0300

    Initial State implemented

--- src/App.js
+++ src/App.js
@@ -3,46 +3,89 @@ import './App.css';
 
 import initialImage from './img/meme-initial.jpg';
 
-function App() {
-  return (
-    <div className="react-meme-generator">
-        <h1>I Can Has Memes?</h1>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <div className="react-meme-generator__form">
-          <input
-            type="text"
-            className="react-meme-generator__input"
-            placeholder="Text Top"
-          />
+    this.state = {
+      topText: '',
+      bottomText: '',
+      memeImage: initialImage,
+    };
+  }
 
-          <input
-            type="text"
-            className="react-meme-generator__input"
-            placeholder="Text Bottom"
-          />
+  render() {
+    const { topText, bottomText, memeImage } = this.state;
 
-          <br/>
+    return (
+      <div className="react-meme-generator">
+          <h1>I Can Has Memes?</h1>
 
-          <button className="react-meme-generator__button">
-            Change picture
-          </button>
+          <div className="react-meme-generator__form">
+            <input
+              value={topText}
+              onChange={(event) => {
+                this.setState({ topText: event.target.value })
+              }}
+              type="text"
+              className="react-meme-generator__input"
+              placeholder="Text Top"
+            />
 
-          <button className="react-meme-generator__button">
-            Load image
-          </button>
+            <input
+              value={bottomText}
+              onChange={(event) => {
+                this.setState({ bottomText: event.target.value })
+              }}
+              type="text"
+              className="react-meme-generator__input"
+              placeholder="Text Bottom"
+            />
 
-          <button className="react-meme-generator__button">
-            Generate
-          </button>
-        </div>
+            <br/>
 
-        <img
-          className="react-meme-generator__meme"
-          src={initialImage}
-          alt=""
-        />
-    </div>
-  );
+            <button className="react-meme-generator__button">
+              Change picture
+            </button>
+
+            <button className="react-meme-generator__button">
+              Load image
+            </button>
+
+            <button className="react-meme-generator__button">
+              Generate
+            </button>
+          </div>
+
+
+
+
+          <div className="react-meme-generator__meme-container">
+            <h2
+              className="
+                react-meme-generator__meme-text
+                react-meme-generator__meme-text--top
+              "
+            >
+              {topText}
+            </h2>
+
+            <h2
+              className="
+                react-meme-generator__meme-text
+                react-meme-generator__meme-text--bottom
+              "
+            >
+              {bottomText}
+            </h2>
+
+            <img
+              className="react-meme-generator__meme"
+              src={memeImage}
+              alt=""
+            />
+          </div>
+      </div>
+    );
+  }
 }
-
-export default App;
