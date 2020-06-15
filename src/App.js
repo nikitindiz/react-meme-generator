15a92128d978cff7e45c9835089b1ab40fb1ec45
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-15 10:57:29 +0300

    Initial JSX is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import initialImage from './img/meme-initial.jpg';
 
 function App() {
   return (
-    <div className="App">
-        React Meme Generator
+    <div className="react-meme-generator">
+        <h1>I Can Has Memes?</h1>
+
+        <div className="react-meme-generator__form">
+          <input
+            type="text"
+            className="react-meme-generator__input"
+            placeholder="Text Top"
+          />
+
+          <input
+            type="text"
+            className="react-meme-generator__input"
+            placeholder="Text Bottom"
+          />
+
+          <br/>
+
+          <button className="react-meme-generator__button">
+            Change picture
+          </button>
+
+          <button className="react-meme-generator__button">
+            Load image
+          </button>
+
+          <button className="react-meme-generator__button">
+            Generate
+          </button>
+        </div>
+
+        <img
+          className="react-meme-generator__meme"
+          src={initialImage}
+          alt=""
+        />
     </div>
   );
 }
